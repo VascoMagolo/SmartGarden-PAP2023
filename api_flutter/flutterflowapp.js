4691f2be80943b57e24749d5d9c93e2e85ae964c
Vasco Magolo <vascomagolo@gmail.com>
2023-06-14 14:23:47 +0100

    updated

--- api_flutter/flutterflowapp.js
+++ api_flutter/flutterflowapp.js
@@ -20,36 +20,35 @@ app.post('/tryLogin', function (req, res) {
         database: 'j65crs1a_smart'
     });
     dbAccess.connect();
-
     dbAccess.query(`SELECT users.IDuser,users.login,users.pass,users.nome FROM users WHERE users.login = '${req.body.username}' AND users.pass = MD5('${req.body.password}')`, function (error, results, fields) {
         if (error) return console.log(error);
         if(results.length === 1) {
             dbAccess.end();
             iduser=results[0].IDuser;
-            res.status(200).send({
-                "IDUSER": results[0].IDuser
-            });
+             if (iduser!==0){
+                dbAccess.query(`SELECT dashboard FROM dashboard WHERE dashboard.iduser = '${iduser}'`, function (error, results2, fields) {
+                    if (error) throw error;
+                    if(results2.length === 1) {
+                        dbAccess.end();
+                        res.status(200).send({
+                            "DASHBOARD": results2[0].dashboard,
+                            "IDUSER": results[0].IDuser
+                        });
+                    } else {
+                        dbAccess.end();
+                        res.status(500);
+                    }
+                });
+             }else{
+                 res.status(200).send({
+                     "IDUSER": results[0].IDuser
+                 });
+             }
         } else {
             dbAccess.end();
             res.status(500);
         }
     });
-    
-    if (iduser!==0){
-        dbAccess.connect();
-        dbAccess.query(`SELECT dashboard FROM dashboard WHERE dashboard.iduser = '${iduser}'`, function (error, results, fields) {
-            if (error) throw error;
-            if(results.length === 1) {
-                dbAccess.end();
-                res.status(200).send({
-                    "DASHBOARD": results[0].dashboard
-                });
-            } else {
-                dbAccess.end();
-                res.status(500);
-            }
-        });
-    }
 });
 
 app.listen(3000);
