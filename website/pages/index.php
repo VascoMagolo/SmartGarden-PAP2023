5fca634713b41029e77b898bf2fe6ac05bbf94ac
Vasco Magolo <vascomagolo@gmail.com>
2023-06-15 17:58:43 +0100

    Update index.php

--- website/pages/index.php
+++ website/pages/index.php
@@ -7,9 +7,7 @@ include("../pages/nav.php");
 <body>   
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../css/styleImportant.css">
+<link rel="stylesheet" href="../css/styleImportant.css">
 <style>#Home{color: #0cd268;}</style>
     <div class="banner_section">
             <div class="container-fluid">
