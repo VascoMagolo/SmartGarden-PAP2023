ef56ebee851c43233a88863505db20c71ceda2fd
Vasco Magolo <vascomagolo@gmail.com>
2023-06-16 13:53:47 +0100

    Update nav.php

--- website/pages/nav.php
+++ website/pages/nav.php
@@ -20,7 +20,6 @@
 </style>
 <nav class="navbar navbar-expand-sm navbar-light bg-dark" id='navbartoggle'style='min-height:5rem;height:auto;'>
     <div class="container-fluid">
-
         <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-list" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
