988facdca10e7a31ba14a9f55f8a5cf12fcfa25d
Vasco Magolo <vascomagolo@gmail.com>
2023-06-23 17:34:42 +0100

    Update flutterflowapp.js

--- api_flutter/flutterflowapp.js
+++ api_flutter/flutterflowapp.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express();
 const mysql = require('mysql');
 let iduser=0;
+let IDdash=0;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,6 +15,7 @@ app.post('/tryLogin', function (req, res) {
         database: 'j65crs1a_smart'
     });
     dbAccess.connect();
+    const date =new Date().toJSON().slice(0,10);
     const query=`
         SELECT 
             users.IDuser,
@@ -27,22 +29,41 @@ app.post('/tryLogin', function (req, res) {
     
     const query2=`
         SELECT 
+            IDdash,
             dashboard 
         FROM 
             dashboard 
         WHERE 
             dashboard.iduser = '${iduser}'`
-
+    const query3=`
+        SELECT
+            data.ID,
+            data.Device_ID,
+            data.App_ID,
+            data.Date,
+            data.Temperature,
+            data.Humidity,
+            data.TemperaturaSolo,
+            data.HumidadeSolo,
+            data.Relay,
+            data.IDdash
+        FROM
+            data
+        WHERE
+            (data.Device_ID LIKE "eui-ac1f09fffe08e925") AND (data.Date LIKE "%'${date}'%") AND (data.IDdash='${IDdash}');`
         dbAccess.query(query, function (error, results, fields) {
         if(results.length === 1) {
             iduser=results[0].IDuser;
             if (iduser!==0){
                 dbAccess.query(query2, function (error, results2, fields) {
                     if(results2.length === 1) {
-                        dbAccess.end();
-                        res.status(200).send({
-                            "DASHBOARD": results2[0].dashboard,
-                            "User": results
+                        IDdash=results2[0].IDdash;
+                        dbAccess.query(query2, function (error, results3, fields) {
+                            dbAccess.end();
+                            res.status(200).send({
+                                "DASHBOARD": results2[0].dashboard,
+                                "User": results
+                            });
                         });
                     } else {
                         dbAccess.end();
