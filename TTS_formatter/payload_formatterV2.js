f086f422b8ed3b3fc24bf31b519bc85774f5183b
Vasco Magolo <vascomagolo@gmail.com>
2023-09-20 11:52:18 +0100

    added a new version of the formatter

---
+++ TTS_formatter/payload_formatterV2.js
@@ -0,0 +1,32 @@
+function Decoder(bytes, port) {
+    let hexa=[];
+    let deca=[];
+    for (let i=0;i<bytes.length;i++){
+      hexa[i]=bytes[i].toString(16);
+      deca[i]=parseInt(hexa[i], 16);
+    }
+    if (deca[2]===2){
+      return{
+          Temperature:deca[0],
+          Humidity:deca[1],
+      };
+    }
+    else if(deca[2]===1){
+      return {
+          Temperature:deca[0],
+          Humidity:deca[1],
+          TempS:deca[3],
+          HumS:deca[4],
+          Relay:"On"
+        };   
+    }
+    else if(deca[2]===0){
+      return {
+          Temperature:deca[0],
+          Humidity:deca[1],
+          TempS:deca[3],
+          HumS:deca[4],
+          Relay:"Off"
+        };   
+  }
+  }
