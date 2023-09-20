02e78f541652d8989dc70befa981301315b8bdbc
Vasco Magolo <vascomagolo@gmail.com>
2023-09-20 11:54:46 +0100

    Update payload_formatterV2.js

--- TTS_formatter/payload_formatterV2.js
+++ TTS_formatter/payload_formatterV2.js
@@ -2,31 +2,31 @@ function Decoder(bytes, port) {
     let hexa=[];
     let deca=[];
     for (let i=0;i<bytes.length;i++){
-      hexa[i]=bytes[i].toString(16);
-      deca[i]=parseInt(hexa[i], 16);
+        hexa[i]=bytes[i].toString(16);
+        deca[i]=parseInt(hexa[i], 16);
     }
     if (deca[2]===2){
-      return{
-          Temperature:deca[0],
-          Humidity:deca[1],
-      };
+        return{
+            Temperature:deca[0],
+            Humidity:deca[1],
+        };
     }
     else if(deca[2]===1){
-      return {
-          Temperature:deca[0],
-          Humidity:deca[1],
-          TempS:deca[3],
-          HumS:deca[4],
-          Relay:"On"
+        return {
+            Temperature:deca[0],
+            Humidity:deca[1],
+            TempS:deca[3],
+            HumS:deca[4],
+            Relay:"On"
         };   
     }
     else if(deca[2]===0){
-      return {
-          Temperature:deca[0],
-          Humidity:deca[1],
-          TempS:deca[3],
-          HumS:deca[4],
-          Relay:"Off"
+        return {
+            Temperature:deca[0],
+            Humidity:deca[1],
+            TempS:deca[3],
+            HumS:deca[4],
+            Relay:"Off"
         };   
-  }
-  }
+    }
+}
